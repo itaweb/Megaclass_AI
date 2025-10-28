cc0ac103df438b4fb8f2b82324d43c2ac982725a
itaweb <itaweb@mail.ru>
2025-10-28 12:48:06 +0300

    Базовый синтаксис языка Python

---
+++ 01_AI.py
@@ -0,0 +1,19 @@
+# целые числа и числа с плавающей точкой
+age = 25
+height = 5.9
+
+# строки
+name = "Alice"
+greeting = "Hello, " + name
+
+# список и кортеж
+numbers = [1, 2, 3, 4]
+names = ["Alice", "Jack"]
+coordinates = (10, 20)
+
+# словарь
+person  = {"name": "Alice", "age": 25}
+
+# Булевы значения
+is_student = True
+is_warior = False
