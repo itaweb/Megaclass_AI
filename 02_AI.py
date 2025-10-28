c5ed4f2f2854a99ca2940f3a45c6b0de26feab81
itaweb <itaweb@mail.ru>
2025-10-28 13:14:26 +0300

    Печать и действия с переменными

---
+++ 02_AI.py
@@ -0,0 +1,22 @@
+# упражнение номер 1 первого дня обучения
+print("Hello, AI World!")
+
+# упражнение номер 2 второго дня обучения
+# определение переменных и их типов
+integer_var = 10
+float_var = 3.14
+string_var = "AI"
+list_var = [1, 2, 3]
+tuple_var = (4, 5, 6)
+dict_var = {"name": "Alice", "role": "Engineer"}
+bool_var = True
+
+# печать и действия с переменными
+print("Целое число: ", integer_var)
+print("Дробное число: ", float_var)
+print("Строка: ", string_var + " Bootcamp")
+list_var.append(4)
+print("Список: ", list_var)
+print("Кортеж: ", tuple_var)
+print("Значение 'name' в словаре: ", dict_var["name"])
+print("Булево значение: ", bool_var)
