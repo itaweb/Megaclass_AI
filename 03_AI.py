230cb99aea5fe7d6aa6e7336e9d04865f0a904ee
itaweb <itaweb@mail.ru>
2025-10-29 22:09:13 +0300

    Синтаксис и примеры операторов управления потоками Python

---
+++ 03_AI.py
@@ -0,0 +1,46 @@
+# Обратный отсчёт от пяти
+# count = 5
+# while count  > 0:
+#   print(count)
+#   count -= 1
+
+
+# Синтаксис для цикла while
+# while True:
+#   # Code block
+
+
+# Цикл для списков
+# fruits = ["яблоко", "банан", "вишня"]
+# for fruit in fruits:
+#   print(fruit)
+  
+# # Цикл для диапазона
+# for i in range(5): # [0, 1, 2, 3, 4]
+#   print(i)
+
+
+# Синтаксис для цикла
+
+# for items in sequense:
+    # Code block
+
+
+# Пример 1: Условие использующее поток управления
+
+# num = 10
+# if num > 0:
+#   print("Положительное число")
+# elif num == 0:
+#   print("Данное число - ноль")
+# else:
+#   print("Это отрицательное число")
+  
+# Пример 2: Вложенные условия
+
+# age == 25
+# if age > 18:
+#   if age < 30:
+#     print("Это молодой человек")
+#   else:
+#     print("Взрослый человек")
